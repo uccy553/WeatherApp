d922decf88eb74eee7be38d4861cce70a73e0da3
uccy553 <umonday98@gmail.com>
2022-09-11 10:53:03 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,85 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import hotbg from './assets/hot.jpg';
+import coldbg from './assets/cold.jpg';
+import Descriptions from './components/Descriptions';
+import { getFormattedWeatherData } from './WeatherService';
+import {BsSearch} from 'react-icons/bs';
+
 
 function App() {
+  const [city, setCity] = useState("Arizona");
+  const [weather, setWeather] = useState(null);
+  const [units, setUnits] = useState('metric');
+  const [bg, setBg] = useState(hotbg);
+
+
+  useEffect(() => {
+    const fetchWeatherData = async () => {
+      const data = await getFormattedWeatherData(city, units);
+      setWeather(data);
+
+      const threshold = units === 'metric' ? 20 : 60;
+      if(data.temp <= threshold) setBg(coldbg)
+      else setBg(hotbg)
+    };
+    fetchWeatherData();
+  }, [units, city]);
+
+  const handleUnitClick = (event) => {
+    const button = event.currentTarget;
+    const currrentUnit = button.innerText.slice(1);
+    
+    const isCelsius = currrentUnit === 'C';
+    button.innerText = isCelsius ? '°F' : '°C'
+    setUnits(isCelsius ? 'metric' : 'imperial')
+  }
+  
+  const enterKeyPress = (e) => {
+    if (e.keyCode === 13) {
+      setCity(e.currentTarget.value)
+      e.currentTarget.blur()
+
+    }
+  }
+
+  console.log(city)
+
+  const handleSearch = () => {
+    if(city !== "Arizona") {
+      setCity(city);
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app" style={{ backgroundImage: `url(${bg})`, backgroundSize: 'cover', backgroundPosition: 'center', height: 'fit-content' }}>
+      <div className='overlay'>
+        {
+          weather && (
+            <div className='container'>
+            <div className='section section__inputs'>
+              <input onKeyDown={enterKeyPress} onChange={(e) => setCity(e.currentTarget.value)} type="text" name="city" placeholder='Enter City...' />
+              <button className='btn' onClick={handleSearch} ><BsSearch /></button>
+              <button className='btn-1' onClick={(event) => handleUnitClick(event)}>°F</button>
+            </div>
+  
+            <div className='section section__temperature'>
+              <div className='icon'>
+                <h3>{`${weather.name}, ${weather.country}`}</h3>
+                <img src={weather.iconURL} alt="sun" width={80} />
+                <h3>{weather.description}</h3>
+              </div>
+  
+              <div className='temperature'>
+                <h1>{`${weather.temp.toFixed()} °${units === 'metric' ? "C" : "F"}`}</h1>
+              </div>
+            </div>
+  
+            {/* bottom description */}
+            <Descriptions weather={weather} units={units} />
+          </div>
+          )
+        }
+      </div>
     </div>
   );
 }
---
+++ src/WeatherService.js
@@ -0,0 +1,19 @@
+const API_KEY = '5b7e1213595f7878f933c3ce7e9c7b69';
+
+const makeIconURL = (iconId) => `https://openweathermap.org/img/wn/${iconId}@2x.png`
+
+const getFormattedWeatherData = async (city, units = 'metric') => {
+    const URL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=${units}`;
+
+    const data = await fetch(URL).then((res) => res.json()).then((data) => data);
+
+    const { weather, main: {temp, feels_like, temp_min, temp_max, pressure, humidity }, wind: { speed }, sys: {country}, name, } = data;
+
+    const { description, icon } = weather[0];
+
+    return {
+        description, iconURL: makeIconURL(icon), temp, feels_like, temp_max, temp_min, pressure, humidity, speed, country, name
+    }
+}
+
+export {getFormattedWeatherData}
